60b4391c7bb9e7644ceee867a0c002cc6afd7c74
ilya-bkv <i.yablokov@ccmp.me>
2025-07-01 12:01:25 +0300

    Update uploader

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import './App.css'
 import { ReceiptUploader } from './components/ReceiptUploader.tsx';
-import { Stack, PinInput, Text, Title, Button } from '@mantine/core';
+import {
+  Stack,
+  PinInput,
+  Text,
+  Title,
+  Button,
+  Badge,
+  Group,
+  CloseButton,
+  Avatar,
+  NumberFormatter
+} from '@mantine/core';
 import { useEffect, useState } from 'react';
 import { useTonConnectUI, useTonAddress } from '@tonconnect/ui-react';
 import type { JettonBalance } from '@ton-api/client';
-import ta from "./tonapi";
+import ta from './tonapi';
 import { Address } from '@ton/core';
 
-const  shortenAddress = (address: string, startLen = 6, endLen = 3): string => {
+const shortenAddress = (address: string, startLen = 6, endLen = 3): string => {
   if (address.length <= startLen + endLen + 3) return address;
   return `${address.slice(0, startLen)}…${address.slice(-endLen)}`;
 }
@@ -16,7 +27,7 @@ function App() {
   const [pin, setPin] = useState<string>('');
   const [isValid, setIsValid] = useState<boolean | null>(null);
   const [isTonConnected, setIsTonConnected] = useState(false);
-  const [jettons, setJettons] = useState<JettonBalance[] | null>(null);
+  const [jetton, setJetton] = useState<JettonBalance | null>(null);
 
   const [tonConnectUI] = useTonConnectUI();
   const connectedAddressString = useTonAddress();
@@ -57,7 +68,7 @@ function App() {
       try {
         await tonConnectUI.openModal();
       } catch (error) {
-        console.error("Ton connect error:", error);
+        console.error('Ton connect error:', error);
       }
       return;
     }
@@ -65,18 +76,34 @@ function App() {
 
   useEffect(() => {
     if (!connectedAddressString) {
-      setJettons(null);
+      setJetton(null);
       return;
     }
 
     ta.accounts
       .getAccountJettonsBalances(Address.parse(connectedAddressString))
-      .then(res => setJettons(res.balances))
-      .catch(err => console.error(err.message || "Failed to fetch jettons"));
+      .then(res => {
+        console.log('%c!!! getAccountJettonsBalances:', 'color: #bada55', res)
+        const chk = res.balances.find((item: JettonBalance) => item.jetton.symbol === 'CHK');
+        if (chk) setJetton(chk)
+      })
+      .catch(err => console.error(err.message || 'Failed to fetch jettons'));
   }, [connectedAddressString]);
 
   return (
     <>
+      {isValid === true && isTonConnected && tonConnectUI.wallet && (
+        <Group p={12} justify="right" w="100%" gap="xs" className="sticky-header">
+          <Badge
+            size="lg"
+            variant="gradient"
+            gradient={{from: 'indigo', to: 'cyan', deg: 90}}
+          >
+            {shortenAddress(connectedAddressString)}
+          </Badge>
+          <CloseButton onClick={() => tonConnectUI.disconnect()}/>
+        </Group>
+      )}
       <Stack
         bg="var(--mantine-color-body)"
         align="center"
@@ -85,26 +112,28 @@ function App() {
       >
         <Title order={1} ta="center">Cheeki Earn</Title>
 
-        {isTonConnected && tonConnectUI.wallet && (
-          <Stack align="center">
-            <Text fw={700}>{shortenAddress(connectedAddressString)}</Text>
-            <Button size="xs" color="red" onClick={() => tonConnectUI.disconnect()}>
-              Disconnect
-            </Button>
-
-            {jettons ? (
-              jettons.map((j, index) => (
-                <Text size="sm" key={index}>
-                  {j.jetton.name}: {j.balance}
-                </Text>
-              ))
-            ) : (
-              <p>No jettons found</p>
-            )}
+        {isValid === true && jetton && jetton.balance > 0 ? (
+          <Stack gap="xs" mt={24}>
+            <Text size="sm">
+              Your goals:
+            </Text>
+            <Group gap="xs">
+              <Avatar size="sm" src={jetton.jetton.image} alt={jetton.jetton.symbol}>
+                {jetton.jetton.symbol}
+              </Avatar>
+              <Text size="lg" fw={500}>
+                <NumberFormatter
+                  suffix={` ${jetton.jetton.symbol}`}
+                  value={Number(jetton.balance)}
+                  thousandSeparator
+                />
+              </Text>
+            </Group>
           </Stack>
+        ) : (
+          <img src="/logo.png" alt="Logo" style={{width: '100px', margin: '0 auto'}}/>
         )}
 
-        <img src="/logo.png" alt="Logo" style={{ width: '100px', margin: '0 auto' }} />
         {(isValid === null || !isValid) && (
           <>
             <Text size="lg">
@@ -126,8 +155,8 @@ function App() {
         )}
         {isValid === true && (
           <>
-
-            {isTonConnected ? <ReceiptUploader/> : <Button color="dark" onClick={handleTonClick}>Connect TON Wallet to continue</Button>}
+            {isTonConnected ? <ReceiptUploader/> :
+              <Button color="dark" onClick={handleTonClick}>Connect TON Wallet to continue</Button>}
           </>
         )}
       </Stack>
